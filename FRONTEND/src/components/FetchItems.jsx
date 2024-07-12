3a9ca43716e9c1571f4008452bc1abf21f33d90f
Manoj Lalchand Pal <86287266+Manoj-Pal-21@users.noreply.github.com>
2024-07-12 19:24:23 +0530

    Update FetchItems.jsx
    change the url path in frontend file

--- FRONTEND/src/components/FetchItems.jsx
+++ FRONTEND/src/components/FetchItems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
         const signal = controller.signal;
 
         dispatch(fetchActions.markFetchingStarted());
-        fetch('http://localhost:8080/items', { signal })
+        fetch('http://shopping-app-ptkv/items', { signal })
             .then((res) => res.json())
             .then(({ items }) => {
                 dispatch(fetchActions.markFetchDone());
