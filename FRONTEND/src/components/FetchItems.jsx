0988bfc9b8701e658eabc70cf9638022a56b86d2
Manoj Lalchand Pal <86287266+Manoj-Pal-21@users.noreply.github.com>
2024-07-12 19:27:23 +0530

    Update FetchItems.jsx
    change in fetchItems

--- FRONTEND/src/components/FetchItems.jsx
+++ FRONTEND/src/components/FetchItems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
         const signal = controller.signal;
 
         dispatch(fetchActions.markFetchingStarted());
-        fetch('http://shopping-app-ptkv/items', { signal })
+        fetch('http://shopping-app-ptkv.onrender.com/items', { signal })
             .then((res) => res.json())
             .then(({ items }) => {
                 dispatch(fetchActions.markFetchDone());
